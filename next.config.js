67107661f80738fe229b120cf227527231449e33
Oein <oein0219@gmail.com>
2023-05-06 12:16:01 +0900

    v1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+module.exports = withPWA({
+  dest: "public",
+});
